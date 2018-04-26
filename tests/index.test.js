df3104a8d2c33f32f9dd1e6f94500d48288887be
Maciej Winnicki <mthenw@users.noreply.github.com>
2018-04-26 13:18:34 +0200

    update SDK initialization: extract space from url (#23)
    * use standard as code style
    
    * update SDK initialization
    
    * apikey -> apiKey
    
    * update README
    
    * revert changes in README
    
    * add apiKey requirement
    
    * fix lint

---
+++ tests/index.test.js
@@ -0,0 +1,59 @@
+const SDK = require('../lib/index')
+
+describe('SDK', () => {
+  describe('for standalone', () => {
+    test('should create config based on URL', () => {
+      const sdk = new SDK({
+        url: 'localhost:4000'
+      })
+
+      expect(sdk.config.eventsUrl).toEqual('http://localhost:4000')
+      expect(sdk.config.configurationUrl).toEqual('http://localhost:4001')
+      expect(sdk.config.space).toEqual('default')
+    })
+
+    test('should ignore apiKey from config', () => {
+      const sdk = new SDK({
+        url: 'localhost:4000',
+        apiKey: 'xxx'
+      })
+
+      expect(sdk.config.apiKey).toBeUndefined()
+    })
+  })
+
+  describe('for hosted', () => {
+    test('should create config based on URL', () => {
+      const sdk = new SDK({
+        url: 'test-app.slsgateway.com',
+        apiKey: 'xxx'
+      })
+
+      expect(sdk.config.eventsUrl).toEqual('https://test-app.slsgateway.com')
+      expect(sdk.config.configurationUrl).toEqual('https://config.slsgateway.com')
+      expect(sdk.config.space).toEqual('test-app')
+      expect(sdk.config.apiKey).toEqual('xxx')
+    })
+
+    test('should ignore space and config URL from config', () => {
+      const sdk = new SDK({
+        url: 'test-app.slsgateway.com',
+        apiKey: 'xxx',
+        configurationUrl: 'http://localhost:4001',
+        space: 'hello'
+      })
+
+      expect(sdk.config.space).not.toEqual('hello')
+      expect(sdk.config.configurationUrl).not.toEqual('http://localhost:4001')
+    })
+
+    test('should throw expection if apiKey is not provided', () => {
+      function init () {
+        // eslint-disable-next-line
+        new SDK({ url: 'test-app.slsgateway.com' })
+      }
+
+      expect(init).toThrowError('apiKey is required for configuring SDK for hosted Event Gateway')
+    })
+  })
+})
