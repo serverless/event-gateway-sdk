83b550bc7f5c658712545ba16bb3acf8ffd00af2
Maciej Winnicki <maciej.winnicki@serverless.com>
2018-04-30 14:33:09 +0200

    add debug message

--- tests/utils/eventGatewayDownload.js
+++ tests/utils/eventGatewayDownload.js
@@ -13,6 +13,8 @@ const release = {
 }
 
 const download = target => {
+  console.log(111111111111111111111111111111, process.env.GH_API_KEY)
+
   if (process.env.GH_API_KEY) {
     octokit.authenticate({
       type: 'token',
