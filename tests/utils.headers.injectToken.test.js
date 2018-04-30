be87ab5d31ad6e3a8ce0e37e23051b5c1abe483c
Alex DeBrie <alexdebrie1@gmail.com>
2018-04-30 10:25:38 -0500

    Change apiKey to accessKey (#27)
    * Change apiKey to accessKey
    
    * More apikey --> accesskey changes

--- tests/utils.headers.injectToken.test.js
+++ tests/utils.headers.injectToken.test.js
@@ -1,22 +1,22 @@
 const headersUtils = require('../lib/utils/headers')
 
 afterEach(() => {
-  delete process.env.EVENT_GATEWAY_API_KEY
+  delete process.env.EVENT_GATEWAY_ACCESS_KEY
 })
 
-test('should inject the Authorization header if an API key is provided', () => {
-  const apiKey = 'hjkl-5678'
+test('should inject the Authorization header if an Access key is provided', () => {
+  const accessKey = 'hjkl-5678'
   const headers = {
     'Content-Type': 'application/json'
   }
-  expect(headersUtils.injectKey(headers, apiKey)).toEqual({
+  expect(headersUtils.injectKey(headers, accessKey)).toEqual({
     'Content-Type': 'application/json',
     Authorization: 'bearer hjkl-5678'
   })
 })
 
 test('should inject the Authorization header if serverless application token is present as env variable', () => {
-  process.env.EVENT_GATEWAY_API_KEY = 'wasd-1234'
+  process.env.EVENT_GATEWAY_ACCESS_KEY = 'wasd-1234'
   const headers = {
     'Content-Type': 'application/json'
   }
@@ -27,12 +27,12 @@ test('should inject the Authorization header if serverless application token is 
 })
 
 test('should prefer a given token over the environment variable for Authorization header if both are present', () => {
-  process.env.EVENT_GATEWAY_API_KEY = 'wasd-1234'
-  const apiKey = 'hjkl-5678'
+  process.env.EVENT_GATEWAY_ACCESS_KEY = 'wasd-1234'
+  const accessKey = 'hjkl-5678'
   const headers = {
     'Content-Type': 'application/json'
   }
-  expect(headersUtils.injectKey(headers, apiKey)).toEqual({
+  expect(headersUtils.injectKey(headers, accessKey)).toEqual({
     'Content-Type': 'application/json',
     Authorization: 'bearer hjkl-5678'
   })
