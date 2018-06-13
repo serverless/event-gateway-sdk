6106273255f81347595abb9af6b3f1b4e155c48c
Maciej Winnicki <mthenw@users.noreply.github.com>
2018-06-13 13:12:12 +0200

    Add tests for Event Types methods (#39)

---
+++ tests/eventTypes.test.js
@@ -0,0 +1,67 @@
+const SDK = require('../lib/index')
+const eventGatewayProcess = require('./utils/eventGatewayProcess')
+
+let eventGateway
+let eventGatewayProcessId
+
+beforeAll(() =>
+  eventGatewayProcess
+    .spawn({
+      configPort: 4001,
+      apiPort: 4002
+    })
+    .then(processInfo => {
+      eventGatewayProcessId = processInfo.id
+      eventGateway = new SDK({
+        space: 'testspace',
+        url: `http://localhost:${processInfo.apiPort}`,
+        configurationUrl: `http://localhost:${processInfo.configPort}`
+      })
+    }))
+
+afterAll(() => {
+  eventGatewayProcess.shutDown(eventGatewayProcessId)
+})
+
+const createEventTypeConfig = {
+  name: 'test.event'
+}
+
+const eventTypeConfig = {
+  space: 'testspace',
+  name: 'test.event'
+}
+
+test('should add an event type to the gateway', () => {
+  return eventGateway.createEventType(createEventTypeConfig).then(response => {
+    expect(response).toEqual(eventTypeConfig)
+  })
+})
+
+test('should return list of functions', () => {
+  return eventGateway.listEventTypes().then(response => {
+    expect(response).toEqual([eventTypeConfig])
+  })
+})
+
+test('should fail to re-add the same function', () => {
+  return eventGateway.createEventType(createEventTypeConfig).catch(err => {
+    expect(err).toEqual(
+      new Error('Failed to create test.event event type due the error: Event Type "test.event" already exists.')
+    )
+  })
+})
+
+test('should remove the added function', () => {
+  return eventGateway.deleteEventType({ name: 'test.event' }).then(response => {
+    expect(response).toBeUndefined()
+  })
+})
+
+test('should fail to remove a non-existing function', () => {
+  return eventGateway.deleteEventType({ name: 'non.existing' }).catch(err => {
+    expect(err).toEqual(
+      new Error('Failed to delete non.existing event type due the error: Event Type "non.existing" not found.')
+    )
+  })
+})
