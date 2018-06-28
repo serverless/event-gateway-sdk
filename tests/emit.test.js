8447cfee6a66f39805c5c3c6132ec6f2a82d4fa9
Maciej Winnicki <maciej.winnicki@serverless.com>
2018-06-28 10:31:21 +0200

    Fix handling error response in emit

--- tests/emit.test.js
+++ tests/emit.test.js
@@ -3,12 +3,88 @@ const SDK = require('../lib/index')
 const eventGatewayProcess = require('./utils/eventGatewayProcess')
 const delay = require('./utils/delay')
 
-const requests = []
+let eventGateway
+let eventGatewayProcessId
+
+beforeAll(() =>
+  eventGatewayProcess
+    .spawn({
+      configPort: 4013,
+      apiPort: 4014
+    })
+    .then(processInfo => {
+      eventGatewayProcessId = processInfo.id
+      eventGateway = new SDK({
+        url: `http://localhost:${processInfo.apiPort}`,
+        configurationUrl: `http://localhost:${processInfo.configPort}`
+      })
+    })
+    .then(() => {
+      return eventGateway.createEventType(eventType)
+    })
+    .then(() => {
+      return eventGateway.createFunction(functionConfig)
+    }))
+
+afterAll(() => {
+  eventGatewayProcess.shutDown(eventGatewayProcessId)
+})
+
+test('should invoke the subscribed function when emitting an event', () => {
+  server.listen(serverPort)
+  return eventGateway.subscribe(asyncSubscriptionConfig).then(() => {
+    return eventGateway
+      .emit({
+        cloudEventsVersion: '0.1',
+        eventType: 'test.event',
+        eventID: '1',
+        source: '/services/tests',
+        contentType: 'application/json',
+        data: {
+          foo: 'bar'
+        }
+      })
+      .then(response => {
+        expect(response.status).toEqual(202)
+      })
+      .then(() => {
+        server.close()
+      })
+  })
+})
+
+test('should throw an error if error response returned', () => {
+  serverReturningError.listen(serverPort)
+  return eventGateway.subscribe(syncSubscriptionConfig).then(() => {
+    return eventGateway
+      .emit({
+        cloudEventsVersion: '0.1',
+        eventType: 'test.event',
+        eventID: '1',
+        source: '/services/tests',
+        contentType: 'application/json',
+        data: {
+          foo: 'bar'
+        }
+      })
+      .catch(err => {
+        expect(err).toEqual(
+          new Error('Failed to emit the event test.event due the error: Function call failed. Please check logs.')
+        )
+      })
+      .then(() => {
+        serverReturningError.close()
+      })
+  })
+})
+
 const serverPort = 3336
 const server = http.createServer((request, response) => {
-  requests.push(request)
   response.writeHead(200, { 'Content-Type': 'application/json' })
   response.end(JSON.stringify({ message: 'success' }))
+})
+const serverReturningError = http.createServer((request, response) => {
+  response.writeHead(400, { 'Content-Type': 'application/json' })
 })
 
 const eventType = {
@@ -25,61 +101,14 @@ const functionConfig = {
   }
 }
 
-const subscriptionConfig = {
+const asyncSubscriptionConfig = {
   type: 'async',
   functionId: 'test-emit',
   eventType: 'test.event'
 }
 
-let eventGateway
-let eventGatewayProcessId
-
-beforeAll(() =>
-  eventGatewayProcess
-    .spawn({
-      configPort: 4013,
-      apiPort: 4014
-    })
-    .then(processInfo => {
-      eventGatewayProcessId = processInfo.id
-      eventGateway = new SDK({
-        url: `http://localhost:${processInfo.apiPort}`,
-        configurationUrl: `http://localhost:${processInfo.configPort}`
-      })
-      server.listen(serverPort)
-    })
-    .then(() => {
-      return eventGateway.createEventType(eventType)
-    })
-    .then(() => {
-      return eventGateway.createFunction(functionConfig)
-    })
-    .then(() => {
-      return eventGateway.subscribe(subscriptionConfig)
-    }))
-
-afterAll(done => {
-  eventGatewayProcess.shutDown(eventGatewayProcessId)
-  server.close(() => {
-    done()
-  })
-})
-
-test('should invoke the subscribed function when emitting an event', () => {
-  return eventGateway
-    .emit({
-      cloudEventsVersion: '0.1',
-      eventType: 'test.event',
-      eventID: '1',
-      source: '/services/tests',
-      contentType: 'application/json',
-      data: {
-        foo: 'bar'
-      }
-    })
-    .then(delay(300))
-    .then(response => {
-      expect(requests).toHaveLength(1)
-      expect(response.status).toEqual(202)
-    })
-})
+const syncSubscriptionConfig = {
+  type: 'sync',
+  functionId: 'test-emit',
+  eventType: 'test.event'
+}
